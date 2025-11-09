d77c2041344c839e5ea9aaf06cd67f800820abdf
Fest <brychkapovozka@gmail.com>
2025-11-09 04:33:31 +0100

    Add HTML and mobile CSS.

---
+++ app/(private routes)/profile/ProfilePage.client.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import css from "./ProfilePage.module.css";
+
+import { useRouter } from "next/navigation";
+
+import MessageNoInfo from "@/components/MessageNoInfo/MessageNoInfo";
+
+import UserInfoForm from "@/components/UserInfoForm/UserInfoForm";
+
+import { OrdersList } from "./page";
+
+interface ProfileClientProps {
+    ordersList: OrdersList[]
+}
+
+export default function ProfileClient({ ordersList }: ProfileClientProps) {
+    const router = useRouter();
+    return (
+        <div className="container">
+            <h1 className={css.h1}>Кабінет</h1>
+            <div>
+                <div className={css.infoContainer}>
+                    <h2 className={css.h2}>Особиста інформація</h2>
+                    <UserInfoForm/>
+                </div>
+                <div className={css.ordersContainer}>
+                    <h2 className={`${css.h2} ${css.ordersTitle}`}>Мої замовлення</h2>
+                    {
+                        ordersList.length
+                            ?
+                            <ul>
+                                {ordersList.map(({ date, orderNum, sum, status }) => {
+                                    let ukrStatus: "У процесі" | "Комплектується" | "Виконано" | "Скасовано";
+                                    switch (status) {
+                                        case "processing":
+                                            ukrStatus = "У процесі";
+                                            break;
+                                        case "packing":
+                                            ukrStatus = "Комплектується";
+                                            break;
+                                        case "success":
+                                            ukrStatus = "Виконано";
+                                            break;
+                                        default:
+                                            ukrStatus = "Скасовано";
+                                    }
+
+                                    return <li className={css.order} key={orderNum}>
+                                                <div>
+                                                    <p className={css.date}>{date}</p>
+                                                    <p className={css.orderNum}>№{orderNum}</p>
+                                                </div>
+                                                <div>
+                                                    <p>Сума:</p>
+                                                    <p className={css.sum}>{sum} грн</p>
+                                                </div>
+                                                <div className={css.statusGroup}>
+                                                    <p>Статус:</p>
+                                                    <p className={css.status}>{ukrStatus}</p>
+                                                </div>
+                                            </li>
+                                })}
+                            </ul>
+                            :
+                            <MessageNoInfo text="У вас ще не було жодних замовлень! Мершій до покупок!" buttonText="До покупок" onClick={() => router.push("/goods")} />
+                    }
+                </div>
+            </div>
+            <button className={css.logout} type="button">Вийти з кабінету</button>
+        </div>
+    );
+}
