a89a33c663ee5be9dde33a9ca704175fdcedc73c
Alex <rubashka2021@gmail.com>
2025-11-12 11:59:34 +0200

    Перед вимкненням світла

--- app/(auth routes)/register/page.tsx
+++ app/(auth routes)/register/page.tsx
@@ -1,5 +1,23 @@
-import css from "./RegisterPage.module.css"
+"use client";
+
+import css from "./RegisterPage.module.css";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function RegisterPage() {
-	return <></>
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/auth/login">Вхід</Link>
+          </li>
+          <li>
+            <Link href="/auth/register">Реєстрація</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
