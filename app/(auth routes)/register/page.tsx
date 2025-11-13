1fc837770ccb4d1e959b28863ebab98a2025173b
Alex urban <rubashka2021@gmail.com>
2025-11-13 15:16:09 +0200

    add all

--- app/(auth routes)/register/page.tsx
+++ app/(auth routes)/register/page.tsx
@@ -4,14 +4,33 @@ import css from "./RegisterPage.module.css";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { Formik, Form, Field } from "formik";
 
 import AuthNavigation from "@/components/AuthNavigation/authNavigation";
 
 export default function RegisterPage() {
+
+type OrderFormValues = {
+  name:string;
+  phone:string;
+  password:string;
+}
+
+const initialValues: OrderFormValues = {
+  name: "",
+  phone: "",
+  password: ""
+}
+
   return (
     <div className="container">
       <section className={css.registration_section}>
         <AuthNavigation route="/register"/>
+        <Formik initialValues={initialValues} onSubmit={}>
+          <Form>
+
+          </Form>
+        </Formik>
       </section>
     </div>
   );
