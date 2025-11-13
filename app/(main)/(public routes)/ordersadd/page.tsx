31a81214d6bb8289e595e6e509301edc1ae728a7
staffm3 <staffm37@gmail.com>
2025-11-13 19:17:45 +0200

    orders add

---
+++ app/(main)/(public routes)/ordersadd/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useBasketStore } from "@/lib/stores/basketStore"
+import { useEffect, useState } from "react"
+
+export default function OrdersAdd() {
+	const [isAdded, setIsAdded] = useState<boolean>(false)
+	const setBasket = useBasketStore(state => state.setBasket)
+
+	useEffect(() => {
+		const add = () => {
+			if (!isAdded) {
+				setBasket([
+					{
+						id: "6877b9f116ae59c7b60d0107",
+						size: "M",
+						color: "white",
+						amount: 1,
+					},
+					{
+						id: "6877b9f116ae59c7b60d0112",
+						size: "M",
+						color: "black",
+						amount: 5,
+					},
+					{
+						id: "6877b9f116ae59c7b60d0160",
+						size: "M",
+						color: "black",
+						amount: 9,
+					},
+				])
+				setIsAdded(true)
+			}
+		}
+
+		add()
+	}, [isAdded, setBasket])
+	return <p>Added</p>
+}
